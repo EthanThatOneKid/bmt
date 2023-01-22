79fe1b8e30bae364120dabc43f82711f8eb5548c
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-01-22 10:04:40 -0800

    Add `append_element` example

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
-export { parse } from "https://deno.land/std@0.171.0/flags/mod.ts";
-export { join } from "https://deno.land/std@0.171.0/path/posix.ts";
-export { expandGlob } from "https://deno.land/std@0.171.0/fs/mod.ts";
+export { parse } from "https://deno.land/std@0.173.0/flags/mod.ts";
+export { join } from "https://deno.land/std@0.173.0/path/posix.ts";
+export { expandGlob } from "https://deno.land/std@0.173.0/fs/mod.ts";
 
-export type { BuildOptions } from "https://deno.land/x/esbuild@v0.16.17/mod.js";
-export { build, transform } from "https://deno.land/x/esbuild@v0.16.17/mod.js";
+export type { BuildOptions } from "https://deno.land/x/esbuild@v0.17.4/mod.js";
+export { build, transform } from "https://deno.land/x/esbuild@v0.17.4/mod.js";
 export { httpImports } from "https://deno.land/x/esbuild_plugin_http_imports@v1.2.4/index.ts";
 
 export { default as deslash } from "https://deno.land/x/slash@v0.3.0/mod.ts";
---
+++ examples/append_element/README.md
@@ -0,0 +1,20 @@
+# Append element
+
+This is a simple bookmarklet. It will append a new element to the body of the
+page.
+
+This bookmarklet was build using the following command:
+
+```bash
+deno run -A --reload https://deno.land/x/bmt/main.ts ./examples/append_element/main.ts
+```
+
+## Usage
+
+Copy and paste the following into your browser's address bar:
+
+```
+javascript:(()%3D%3E%7Bt(n())%3Bfunction%20n()%7Blet%20e%3Ddocument.createElement(%22div%22)%3Breturn%20e.innerHTML%3D%22Hello%2C%20World!%22%2Ce%7Dfunction%20t(e)%7Bdocument.body.insertBefore(e%2Cdocument.body.firstChild)%7D%7D)()%3B%0A
+```
+
+> Note: For reusability, you may want to add this to your bookmarks bar.
